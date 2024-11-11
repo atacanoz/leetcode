2bfbda2bbf3d45aa07b41ac6fa98fa2006e9c937
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-11 18:45:49 +0300

    Update productsofarraydiscludingself.py

--- productsofarraydiscludingself.py
+++ productsofarraydiscludingself.py
@@ -1,28 +1,25 @@
 nums = [1,2,4,6]
 
+res = list()
+right , left = 1 , 1
 
-lst = list()
 
-# for i ,j in range(len(nums)):
-#     right = right * nums[len(nums)-1]
-#     left = left * nums[i-1]
- 
-# for p in range(len(nums)):
-#     res,right , left ,i= 1,1 , 1 , 0
-#     while p + i + 1 < len(nums):
-#         right = right * nums[p+i+1]
-#         left = left * nums[p-i-1]
-#         res = right * left * nums[p-i-1]
-#         i+=1
-#     lst.append(res)    
-
-# print(lst)
-  
-res=1
-print(len(nums))
-for p in range(len(nums)):
-    res *= nums[p]
+for i in range(len(nums)):
+    res.append(right)
+    right *= nums[i]
     print(res)
+
+for k in range(len(nums)):
+    res[len(nums)-k-1] *= left
+    left *= nums[len(nums)-k-1]
+    print(res)
+ 
+ return res
+    
+    
+
+  
+
 
    
     
@@ -32,5 +29,5 @@ for p in range(len(nums)):
 
 
 
-
+        
 

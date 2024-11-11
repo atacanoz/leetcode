5883eb76591607792a5dee062553dd9330a59b61
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-11 19:29:36 +0300

    Update validsudoku.py

--- validsudoku.py
+++ validsudoku.py
@@ -1,3 +1,5 @@
+from collections import defaultdict 
+
 board = [["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
@@ -8,16 +10,16 @@ board = [["5","3",".",".","7",".",".",".","."]
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]]
 
-x ,y= dict(), dict()
-rows , column= dict(), dict()
+cols = defaultdict(set)
+rows = defaultdict(set)
+squares = defaultdict(set) # key = (r/3 , c/3)
 
-print((len(board)))
 
-for i in range(len(board)):
-    for k in range(len(board)):
-        if board[i][k] not in x and board[i][k] != ".":
-            x[k]=board[i][k]
-    rows[i] = x[k]
+# for i in range(len(board)):
+#     for k in range(len(board)):
+#         if board[i][k] not in x and board[i][k] != ".":
+#             x[k]=board[i][k]
+#     rows[i] = x[k]
 
 
  

363319166a8f86f51e3b0cc4f3662ad200686c7a
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-12 14:06:51 +0300

    Add files via upload

---
+++ validPalindrome.py
@@ -0,0 +1,29 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+s = "A man, a plan, a canal: Panama"
+print(s)
+s_alpha = ""
+s_check = ""
+stack = []
+
+for c in s:
+    if c.isalpha():
+        s_alpha += c.lower()
+        stack.append(c.lower())
+
+while stack:
+    s_check += stack.pop()
+
+
+if s_check == s_alpha:
+    print("TRUE")
+
+print("FALSE")    
+
+
+         

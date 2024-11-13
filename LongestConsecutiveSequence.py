5e550f64b40a0e625f3689df395f57fdb6061f0d
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-13 20:57:11 +0300

    time complexity reduced to O(n)

--- LongestConsecutiveSequence.py
+++ LongestConsecutiveSequence.py
@@ -1,6 +1,5 @@
-import heapq
 
-nums = [0,3,7,2,5,8,4,6,0,1]
+nums = [100,4,200,1,3,2]
 
 # Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 # You must write an algorithm that runs in O(n) time.
@@ -9,32 +8,24 @@ nums = [0,3,7,2,5,8,4,6,0,1]
 # Output: 4
 # Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 
+longest = 0 
+x = set(nums)
 
-heapq.heapify(nums)
-print(nums)
-longest_count = 1
-count = 1
-
-
-while nums:
-
-    x = heapq.heappop(nums)
-    if nums:
-        front = nums[0]
-        if front == (x + 1):
+for n in x:
+    if n-1 not in x:
+        count = 1
+        print(f"begining of seq: {n}")
+        while n+1 in x:
+            n+=1
             count+=1
-        else:
-            if count > longest_count:
-                longest_count = count
-            count=1
-    else:
-        if count > longest_count:
-            longest_count = count     
-                
-                   
+        if count > longest:
+            longest = count    
+            
 
-print(longest_count)
- 
-        
+print(longest)
+       
 
-    
+
+
+
+

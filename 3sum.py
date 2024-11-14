e56c489658c544e1ba6f01ce54c5cb509c567f93
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-14 15:23:46 +0300

    two pointer sort method

--- 3sum.py
+++ 3sum.py
@@ -14,16 +14,30 @@
 # Notice that the order of the output and the order of the triplets does not matter.
 
 nums = [-1, 0, 1, 2, -1, -4]
+nums.sort()
+res = list()
 
-x = set()
+# [-4, -1, -1, 0, 1, 2]
 
-for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
 
-        required = -nums[i] - nums[j]
-        
-        if required in nums and nums.index(required) != i and nums.index(required) != j:
-            triplet = tuple(sorted([nums[i], nums[j], required]))
-            x.add(triplet)
 
-print(x)
+for i in range(len(nums)-2):
+    if nums[i] == nums[i-1]:
+        continue
+    j = i + 1
+    k = len(nums)-1 
+
+    while j < k:
+        total = nums[i] + nums[j] + nums[k]
+        if total > 0:
+            k-=1
+        elif total < 0:
+            j+=1
+        else:
+            triplet = [nums[i],nums[j],nums[k]]   
+            res.append(triplet)
+            j+=1
+       
+                
+print(res)
+

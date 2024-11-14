7f1198e9666c12a2478073c3ac9f32bde637c7ae
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-14 22:48:04 +0300

    Update TrappingRainWater.py

--- TrappingRainWater.py
+++ TrappingRainWater.py
@@ -2,17 +2,39 @@
 # compute how much water it can trap after raining.
 
 
-heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-max_height = max(heights)
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 
-# Initialize the matrix with '0's
-matrix = [['0' for _ in range(len(heights))] for _ in range(max_height)]
+tmp , area = 0 , 0
+i , j = 0 , len(height) - 1 
 
-# Fill in the '#' based on heights
-for col, height in enumerate(heights):
-    for row in range(max_height - height, max_height):
-        matrix[row][col] = '#'
+print(height)
 
-for row in matrix:
-    print(row)
+while height[i] <= height[i+1]:
+    i+=1    
+
+while height[j] <= height[j-1]:
+    j-=1
+
+while i < j:
+    print(f"Alan: {area} , height[{i}]: {height[i]} , height[{j}]: {height[j]}  ")    
+
+    if height[j] > height[i]:
+        if height[i] > height[i+1]:
+            area += height[i] - height[i+1]
+            height[i+1]= height[i]
+        i+=1
+        
+    else:
+        if height[j] > height[j-1]:
+            area += height[j]-height[j-1]
+            height[j-1]=height[j]
+        j-=1
+           
+print(area)
+    
+
+
+
+    
+
 

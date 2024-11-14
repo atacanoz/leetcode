8738d7248369d76c656f7e987216821215a7225f
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-14 15:36:45 +0300

    while nums[j] == nums[j-1] and j < k : j+=1

--- 3sum.py
+++ 3sum.py
@@ -17,26 +17,29 @@ nums = [-1, 0, 1, 2, -1, -4]
 nums.sort()
 res = list()
 
-# [-4, -1, -1, 0, 1, 2]
-
-
 
 for i in range(len(nums)-2):
-    if nums[i] == nums[i-1]:
+    if i > 0 and nums[i] == nums[i-1]:
         continue
+
     j = i + 1
     k = len(nums)-1 
 
     while j < k:
+
         total = nums[i] + nums[j] + nums[k]
+
         if total > 0:
             k-=1
+
         elif total < 0:
             j+=1
-        else:
-            triplet = [nums[i],nums[j],nums[k]]   
-            res.append(triplet)
+
+        else: 
+            res.append([nums[i],nums[j],nums[k]])
             j+=1
+            while nums[j] == nums[j-1] and j < k:
+                j+=1
        
                 
 print(res)

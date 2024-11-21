db2e3d7cd1cdb0d50d9aa5752c051062e6dddf15
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-21 22:45:01 +0300

    Update and rename vo.py to GenerateParanthesis.py

---
+++ GenerateParanthesis.py
@@ -0,0 +1,33 @@
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+# Example 2:
+
+# Input: n = 1
+# Output: ["()"]
+
+def generateParenthesis(n: int):
+   
+    def dfs(left, right, s):
+        if len(s) == n * 2:
+            res.append(s)
+            return
+        
+        if left < n:
+            dfs(left + 1, right, s + "(")
+
+        if right < left:
+            dfs(left, right + 1, s + ")")    
+
+
+    res = []
+    dfs(0, 0, ""),
+
+    return res
+
+
+n = 4
+print(generateParenthesis(n))

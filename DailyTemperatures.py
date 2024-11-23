ae997eb42e1e30bd254dbcc29e518b2444b2b526
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-23 21:34:34 +0300

    stack optimization

--- DailyTemperatures.py
+++ DailyTemperatures.py
@@ -1,26 +1,12 @@
-# Given an array of integers temperatures represents the daily temperatures, 
-# return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. 
-# If there is no future day for which this is possible, keep answer[i] == 0 instead.
-
-# Input: temperatures = [73,74,75,71,69,72,76,73]
-# Output: [1,1,4,2,1,1,0,0]
-
-temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-
-answer = []
+temperatures = [73,74,75,71,69,72,76,73]
+answer = [0] * len(temperatures)  # Initialize the result list with zeros
+stack = []  # This will store indices of temperatures
 
 for i in range(len(temperatures)):
-    count = 0
-    for k in range(i + 1, len(temperatures)):    
-        count += 1        
-        if temperatures[k] > temperatures[i]:
-            break
-    else: 
-        count = 0           
-        
-        
-    # print(f"{temperatures[i]} , {temperatures[k]} , {i}, {k}" ,end = " ")    
-    # print("")
-    answer.append(count)
+    # While stack is not empty and current temperature is greater than the temperature at the index on top of the stack
+    while stack and temperatures[i] > temperatures[stack[-1]]:
+        prev_day = stack.pop()  # Get the index of the previous day
+        answer[prev_day] = i - prev_day  # Calculate the difference in days
+    stack.append(i)  # Push the current day index onto the stack
 
 print(answer)

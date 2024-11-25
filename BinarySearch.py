4f7056988e64639eff64676c1c0f225e80415891
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-25 13:31:35 +0300

    best binary search algorithm code

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,35 +1,18 @@
-# Given an array of integers nums which is sorted in ascending order, and an integer target
-# Write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
-# You must write an algorithm with O(log n) runtime complexity.
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
 
-# Input: nums = [-1,0,3,5,9,12], target = 9
-# Output: 4
-# Explanation: 9 exists in nums and its index is 4
+        left, right = 0, len(nums) - 1 
 
+        while   left <= right:
+            mid = left + (right - left) // 2
 
-nums = [0,1,2,3,4,5,6,7,8,9,10,11,12]
-target = 13
+            if nums[mid]==target:
+                return mid
 
-def BinarySearch(arr, x):
+            elif nums[mid] < target:
+                left = mid + 1
 
-    low = 0
-    high = len(arr) - 1
+            else:
+                right = mid - 1    
 
-    while low <= high:
-        
-        mid = low + (high - low) // 2
-
-        if arr[mid] == x:
-            return mid
-        
-        elif arr[mid] < x:
-            low = mid + 1
-
-        else:
-            high = mid - 1
-
-    return -1            
-
-res = BinarySearch(nums, target)
-
-print(res)
+        return -1

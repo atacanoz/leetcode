f6cc82773dea557563a67ea157c84940ddaf1671
atacanozcihan <147489039+atacanoz@users.noreply.github.com>
2024-11-25 14:13:17 +0300

    Linear search n^2

--- SearchA2DMatrix.py
+++ SearchA2DMatrix.py
@@ -1,27 +1,10 @@
-# You are given an m x n integer matrix matrix with the following two properties:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
-# Each row is sorted in non-decreasing order.
-# The first integer of each row is greater than the last integer of the previous row.
-# Given an integer target, return true if target is in matrix or false otherwise.
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == target:
+                    return True
+        return False            
 
-# You must write a solution in O(log(m * n)) time complexity.
-
-# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
-# Output: true
-
-
-matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-target = 3
-
-def SearchMatrix(matrix, target):
-
-    for r in range(len(matrix)):
-        for c in range(len(matrix[0])):
-            if matrix[r][c] == target:
-                return True
-    return False    
-
-
-res = SearchMatrix(matrix, target)
-
-print(res)
+  

a48473920b3b02619f056c06d10a53c76fd2541d
Atacan <atacanozcihan7@gmail.com>
2024-12-30 23:04:46 +0300

    dmıafmp

--- BinarySearch.py
+++ BinarySearch.py
@@ -6,7 +6,7 @@ def BinarySearch(arr, target):
 
     while left <= right: 
         mid = (left + right)//2
-#ajnlkhbn 
+#ajnlkhbn     
         if arr[mid]==target: 
             return mid    
         elif arr[mid]>target:

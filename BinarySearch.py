16e1be2ab016ac75e8322d72dcf1522826a1c523
Atacan <atacanozcihan7@gmail.com>
2025-01-17 10:26:33 +0300

    minor changes

--- BinarySearch.py
+++ BinarySearch.py
@@ -13,11 +13,11 @@ def BinarySearch(arr, target):
             right = mid - 1
         else:
             left = mid + 1                   
-    return -1               
+    return -1                  
    
+    
 
 
-   
 eee = [0, 1, 2, 3,  4,  5,  6,  7,  8,  9 ]
 arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
 

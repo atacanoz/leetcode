ebccde1f26baa5e7ce5adc6d84ab5c7fdde2939c
Atacan <atacanozcihan7@gmail.com>
2025-01-20 13:13:25 +0300

    minor changes

--- BinarySearch.py
+++ BinarySearch.py
@@ -21,7 +21,7 @@ def BinarySearch(arr, target):
 eee = [0, 1, 2, 3,  4,  5,  6,  7,  8,  9 ]
 arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
 
-target = 38
+target = 38        
 
 print(BinarySearch(arr,target))
 

f705c8526aa1ce56a15ef05e026431209f775960
Atacan <atacanozcihan7@gmail.com>
2025-02-13 19:07:46 +0300

    mesage

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -57,7 +57,7 @@ class LinkedList:
 list = LinkedList()      
 list.insertAtBeginning(5)
 list.insertAtBeginning(4)
-list.insertAtBeginning(3)        
+list.insertAtBeginning(3)             
 list.insertAtBeginning(2)
 list.insertAtBeginning(1)
 

f621c68f08b8116d6bcd61547c67c12362d7aab8
Atacan <atacanozcihan7@gmail.com>
2025-02-21 23:11:52 +0300

    omfm

--- productsofarraydiscludingself.py
+++ productsofarraydiscludingself.py
@@ -4,13 +4,12 @@ res = list()
 right , left = 1 , 1
 
 
-for i in range(len(nums)):
+for i in range(len(nums)):  
     res.append(right)
     right *= nums[i]
     print(res)
 
-for k in range(len(nums)):       
-    res[len(nums)-k-1] *= left    
+for k in range(len(nums)):          
     left *= nums[len(nums)-k-1]
     print(res)       
           
